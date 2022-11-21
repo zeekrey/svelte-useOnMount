7df78055afcd87964aa6f1dc2dd2efc88046dd6a
zeekrey <34539149+zeekrey@users.noreply.github.com>
2022-11-21 11:52:25 +0100

    add some jsdocs

--- src/lib/useOnMounted.ts
+++ src/lib/useOnMounted.ts
@@ -1,6 +1,11 @@
-const onMounted = (node: Node, callback: (node: Node) => void) => {
-  console.log('Mounted!');
+/**
+ * Calls the provided function once the element mounts.
+ * @example
+ * ```html
+ *  <input use:onMounted={(node) => customValidation(node)} />
+ * ```
+ */
+const onMounted = (node: Node, callback: (node: Node) => void) =>
   callback(node);
-};
 
 export default onMounted;
